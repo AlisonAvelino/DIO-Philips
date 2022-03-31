343769c3dc941f33f44374a3500bafd588f7d58d
Alison Avelino <alisonavelino3133@gmail.com>
2022-03-31 14:31:37 -0300

    Conhecendo a modularização em JavaScript

---
+++ JavaScript/modulos/main.js
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Alison", 23));
+console.log(mostraCidade("Alison", "São Paulo"));
+console.log(mostraHobby("Alison", "Jogar FPS"));
+
